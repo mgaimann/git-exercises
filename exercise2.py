cea7266d3a161dd0de8538446efd3753cea4af8a
Ginger Lau <76407489+gingyy@users.noreply.github.com>
2024-10-07 17:04:55 +0200

    Task 2B: implemented isub to subtract vector from given point (#4)
    Co-authored-by: Mario Gaimann <35263162+mgaimann@users.noreply.github.com>

--- exercise2.py
+++ exercise2.py
@@ -14,6 +14,10 @@ class Point2D:
     def y(self) -> float:
         return self._coordinates[1]
         
+    def __isub__(self, other: Vector) -> None:
+       self._coordinates -= other
+       return self
+
     def __iadd__(self, other: Vector) -> None:
         self._coordinates += other
         return self
@@ -33,7 +37,6 @@ def test_point_vector_addition() -> None:
 
 def test_point_vector_subtraction() -> None:
     point = Point2D(1.0, 2.0)
-    # Task B: make the test below pass (implement __isub__)
     point -= Vector([1.1, 2.2])
     assert isclose(point.x, -0.1)
     assert isclose(point.y, -0.2)
